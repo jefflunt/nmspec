a63d6f49b98ec47579e98c160dd800705c036afc
jefflunt <jefflunt@gmail.com>
2021-12-27 16:21:31 -0600

    some bug fixes to the lambda function

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,12 +2,22 @@ require 'json'
 load './lib/nmspec.rb'
 
 def lambda_handler(event:, context:)
+
+  body =  begin
+            JSON.parse(event['body'])
+          rescue JSON::ParserError
+            return {
+              statusCode: 200,
+              body: JSON.generate({ err: "JSON parse error - POST body does not appear to be valid JSON" })
+            }
+          end
+
   {
     statusCode: 200,
     body: JSON.generate(
       Nmspec::V1.gen({
-        'spec' => event.dig('spec'),
-        'langs' => event.dig('langs'),
+        'spec' => body.dig('spec'),
+        'langs' => body.dig('langs'),
       })
     ),
   }
@@ -22,6 +32,11 @@ rescue => e
 
   {
     statusCode: 200,
-    body: JSON.generate({ err: 'Unexpected exception' }),
+    body: JSON.generate(
+      {
+        err: 'Unexpected exception',
+        payload_received: "#{body.to_s[..100]} ..."
+      }
+    ),
   }
 end
