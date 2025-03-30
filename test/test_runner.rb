b5e4b9086dff63bc6057c83a8c42cb53729b29af
jefflunt <jefflunt@gmail.com>
2025-03-30 09:46:36 -0500

    fix the tests to reference ruby3x and gdscript3x

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -3,8 +3,8 @@ require 'yaml'
 
 require_relative '../lib/nmspec/v1'
 require_relative '../lib/nmspec/parser'
-require_relative '../lib/nmspec/ruby'
-require_relative '../lib/nmspec/gdscript.rb'
+require_relative '../lib/nmspec/ruby3x'
+require_relative '../lib/nmspec/gdscript3x.rb'
 
 Dir['test/**/*_test.rb'].each{|f| load f }
 
@@ -13,12 +13,12 @@ def nmspec_parse_and_load_demo(kind)
 
   parse_result = Nmspec::V1.gen({
     'spec' => nmspec,
-    'langs' => ['ruby'],
+    'langs' => ['ruby3x'],
   })
 
   filename = "generated_code/#{kind}.rb"
   File.open(filename, 'w') do |f|
-    f.puts parse_result.dig('code', 'ruby')
+    f.puts parse_result.dig('code', 'ruby3x')
   end
 
   load filename
